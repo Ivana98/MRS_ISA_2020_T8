2d1a2ef6b06cc38f19fb89dabc154df0cd4d34a6
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-04-06 18:32:23 +0200

    Initial app

---
+++ CCSystem/src/main/java/com/team08/CCSystem/CcSystemApplication.java
@@ -0,0 +1,14 @@
+package com.team08.CCSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class CcSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcSystemApplication.class, args);
+	}
+
+}
---
+++ CCSystem/src/test/java/com/team08/CCSystem/CcSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team08.CCSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
