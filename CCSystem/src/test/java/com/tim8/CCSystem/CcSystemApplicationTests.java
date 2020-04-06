bea1d526ca3a8508367486a48973fa6d61e78581
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-04-06 13:35:23 +0200

    First commit

---
+++ CCSystem/src/test/java/com/tim8/CCSystem/CcSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tim8.CCSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
