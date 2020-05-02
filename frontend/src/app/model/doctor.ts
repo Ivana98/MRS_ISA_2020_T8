5689cd030a886f2b4e3cde9f685d008746a530c9
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-05-02 20:53:23 +0200

    Added new attributes and method.

--- frontend/src/app/model/doctor.ts
+++ frontend/src/app/model/doctor.ts
@@ -1,12 +1,14 @@
-import { IAddress } from './address';
-
 export interface IDoctor {
     firstName: string,
     lastName: string,
     email: string,
     phone: string,
     password: string,
-    address: IAddress //maybe just address i dont know
+    country: string,
+    city: string,
+    street: string,
+    clinic_id: string,
+    specialisation: string
 }
 
 export class Doctor {
@@ -16,30 +18,19 @@ export class Doctor {
         public email: string,
         public phone: string,
         public password: string,
-        public address: IAddress
-    ) {} 
-}
-
-export interface IDoctor2 {
-    firstName: string,
-    lastName: string,
-    email: string,
-    phone: string,
-    password: string,
-    country: string,
-    city: string,
-    street: string
-}
-
-export class Doctor2 {
-    constructor(
-        public firstName: string,
-        public lastName: string,
-        public email: string,
-        public phone: string,
-        public password: string,
         public country: string,
         public city: string,
-        public street: string
+        public street: string,
+        public clinic_id: string,
+        public specialisation: string
     ) {} 
+
+    /* If any of fields are empty return false */
+    public filled(): boolean {
+        if (this.firstName == "" || this.lastName == "" || this.email == "" || this.phone == "" || this.password == "" || this.country == "" || this.city == "" || this.street == "" || this.clinic_id == "" || this.specialisation == "") 
+        {
+            return false;
+        }
+        return true;
+    }
 }
