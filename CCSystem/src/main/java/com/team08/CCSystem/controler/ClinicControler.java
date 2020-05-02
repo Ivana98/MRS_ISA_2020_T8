2d38a79350e1d791adf49920f661f03718a32417
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-05-02 20:49:28 +0200

    adding doctor do database done

--- CCSystem/src/main/java/com/team08/CCSystem/controler/ClinicControler.java
+++ CCSystem/src/main/java/com/team08/CCSystem/controler/ClinicControler.java
@@ -3,11 +3,20 @@
  */
 package com.team08.CCSystem.controler;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.team08.CCSystem.dto.ClinicBasicDTO;
+import com.team08.CCSystem.model.Clinic;
 import com.team08.CCSystem.service.ClinicService;
 
 /**
@@ -21,5 +30,21 @@ public class ClinicControler {
 	
 	@Autowired
 	private ClinicService clinicService;
+	
+	/*
+	 * Load all clinics into list, convert to DTO and return
+	 */
+	@GetMapping(path = "/getAll")
+	public ResponseEntity<List<ClinicBasicDTO>> getAll() {
+		List<Clinic> clinics = clinicService.findAll();
+		
+		// convert list clinics to dto
+		List<ClinicBasicDTO> clinicsDTO = new ArrayList<ClinicBasicDTO>();
+		for (Clinic c : clinics) {
+			clinicsDTO.add(new ClinicBasicDTO(c));
+		}
+		
+		return new ResponseEntity<List<ClinicBasicDTO>>(clinicsDTO, HttpStatus.OK);
+	}
 
 }
