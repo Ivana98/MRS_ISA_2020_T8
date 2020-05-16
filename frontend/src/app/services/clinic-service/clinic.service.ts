f2e5b1aec7d2044ec0438f22bdc1764631e2fd3a
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-05-16 10:04:45 +0200

    new method added for updateing, deleteing, searching..

--- frontend/src/app/services/clinic-service/clinic.service.ts
+++ frontend/src/app/services/clinic-service/clinic.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ICLinicToDisplay } from 'src/app/model/clinic';
+import { ClinicBasic } from 'src/app/model/clinicBasic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClinicService {
 
-  private _url: string = "http://localhost:8080/api/clinics/getAll";
+  private _url: string = "http://localhost:8080/api/clinics/";
 
   constructor(private _httpClient: HttpClient) { }
 
@@ -17,6 +18,14 @@ export class ClinicService {
   */
   public getAll(): Observable<Array<ICLinicToDisplay>>
   {
-    return this._httpClient.get<Array<ICLinicToDisplay>>(this._url);
+    return this._httpClient.get<Array<ICLinicToDisplay>>(this._url + "getAll");
+  }
+
+  public getOne(id) {
+    return this._httpClient.get<ClinicBasic>(this._url + "getOne/" + id);
+  }
+
+  public modify(clinic) {
+    return this._httpClient.put<ClinicBasic>(this._url + "modify", clinic);
   }
 }
--- frontend/src/app/services/medical-room-services/medical-room.service.ts
+++ frontend/src/app/services/medical-room-services/medical-room.service.ts
@@ -7,11 +7,26 @@ import { MedicalRoom } from 'src/app/model/medicalRoom';
 })
 export class MedicalRoomService {
 
-  private _url: string = "http://localhost:8080/api/rooms/save";
+  private _url: string = "http://localhost:8080/api/rooms/";
 
   constructor(private _httpClient: HttpClient) { }
 
   public addRoom(room) {
-    return this._httpClient.post<MedicalRoom>(this._url, room);
+    return this._httpClient.post<MedicalRoom>(this._url + "save", room);
+  }
+
+  /*
+  * get all medical rooms from specific clinic
+  */
+  public getAll(clinicId) {
+    return this._httpClient.get<Array<MedicalRoom>>(this._url + "getAll/" + clinicId);
+  }
+
+  public modifyMedicalRoom(room) {
+    return this._httpClient.put<MedicalRoom>(this._url + "modify", room);
+  }
+
+  public deleteMedicalRoom(id) {
+    return this._httpClient.delete<Number>(this._url + "delete/" + id);
   }
 }
