72b0e2f423fc20cc5b4cf7f34e73a14c30a6aad5
Veljko <51533885+Veljko998@users.noreply.github.com>
2020-05-16 10:08:44 +0200

    small changes

--- frontend/src/app/model/medicalRoom.ts
+++ frontend/src/app/model/medicalRoom.ts
@@ -1,20 +1,22 @@
 export interface IMedicalRoom {
+    id: number,
     room_number: string,
     intervention_type: string,
-    clinic_id: string
+    clinic_id: number
 }
 
 export class MedicalRoom {
     constructor(
+        public id: number,
         public room_number: string,
         public intervention_type: string,
-        public clinic_id: string
+        public clinic_id: number
     ) {}
     
     /* If any of fields empty return false */
     public filled(): boolean {
         
-        if (this.room_number == "" || this.intervention_type == "" || this.clinic_id == "") {
+        if (this.room_number == "" || this.intervention_type == "" || this.clinic_id == 0 || this.clinic_id == null) {
             return false;
         }
         return true;
